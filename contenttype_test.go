7a329fd7ae20f59b5be5011e380cf18e8d03d389
enverbisevac <enver@bisevac.com>
2022-10-23 09:44:46 +0200

    fixed linter errors

--- contenttype_test.go
+++ contenttype_test.go
@@ -1,8 +1,29 @@
-package render
+// Copyright (c) 2022 Enver Bisevac
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+package render_test
 
 import (
 	"net/http"
 	"testing"
+
+	"github.com/enverbisevac/render"
 )
 
 func TestGetContentType(t *testing.T) {
@@ -12,82 +33,82 @@ func TestGetContentType(t *testing.T) {
 	tests := []struct {
 		name string
 		args args
-		want ContentType
+		want render.ContentType
 	}{
 		{
 			name: "tex/plain content type",
 			args: args{
-				s: TextPlain,
+				s: render.TextPlain,
 			},
-			want: ContentTypePlainText,
+			want: render.ContentTypePlainText,
 		},
 		{
 			name: "text/html content type",
 			args: args{
-				s: TextHTML,
+				s: render.TextHTML,
 			},
-			want: ContentTypeHTML,
+			want: render.ContentTypeHTML,
 		},
 		{
 			name: "application/xhtml+xml content type",
 			args: args{
-				s: ApplicationXHTML,
+				s: render.ApplicationXHTML,
 			},
-			want: ContentTypeHTML,
+			want: render.ContentTypeHTML,
 		},
 		{
 			name: "application/json content type",
 			args: args{
-				s: ApplicationJSON,
+				s: render.ApplicationJSON,
 			},
-			want: ContentTypeJSON,
+			want: render.ContentTypeJSON,
 		},
 		{
 			name: "text/javascript content type",
 			args: args{
-				s: TextJavascript,
+				s: render.TextJavascript,
 			},
-			want: ContentTypeJSON,
+			want: render.ContentTypeJSON,
 		},
 		{
 			name: "text/xml content type",
 			args: args{
-				s: TextXML,
+				s: render.TextXML,
 			},
-			want: ContentTypeXML,
+			want: render.ContentTypeXML,
 		},
 		{
 			name: "application/xml content type",
 			args: args{
-				s: ApplicationXML,
+				s: render.ApplicationXML,
 			},
-			want: ContentTypeXML,
+			want: render.ContentTypeXML,
 		},
 		{
 			name: "application/x-www-form-urlencoded content type",
 			args: args{
-				s: ApplicationFormURL,
+				s: render.ApplicationFormURL,
 			},
-			want: ContentTypeForm,
+			want: render.ContentTypeForm,
 		},
 		{
 			name: "text/event-stream content type",
 			args: args{
-				s: TextEventStream,
+				s: render.TextEventStream,
 			},
-			want: ContentTypeEventStream,
+			want: render.ContentTypeEventStream,
 		},
 		{
 			name: "unknown content type",
 			args: args{
 				s: "unknown",
 			},
-			want: ContentTypeUnknown,
+			want: render.ContentTypeUnknown,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := GetContentType(tt.args.s); got != tt.want {
+			if got := render.GetContentType(tt.args.s); got != tt.want {
 				t.Errorf("GetContentType() = %v, want %v", got, tt.want)
 			}
 		})
@@ -101,7 +122,7 @@ func TestGetAcceptedContentType(t *testing.T) {
 	tests := []struct {
 		name string
 		args args
-		want ContentType
+		want render.ContentType
 	}{
 		{
 			name: "happy path",
@@ -112,7 +133,7 @@ func TestGetAcceptedContentType(t *testing.T) {
 					},
 				},
 			},
-			want: ContentTypeJSON,
+			want: render.ContentTypeJSON,
 		},
 		{
 			name: "unknown content type",
@@ -123,12 +144,12 @@ func TestGetAcceptedContentType(t *testing.T) {
 					},
 				},
 			},
-			want: ContentTypeJSON,
+			want: render.ContentTypeJSON,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := GetAcceptedContentType(tt.args.r); got != tt.want {
+			if got := render.GetAcceptedContentType(tt.args.r); got != tt.want {
 				t.Errorf("GetAcceptedContentType() = %v, want %v", got, tt.want)
 			}
 		})
